6495e531021433a9c135cbe8ccf2166900b84c3b
Angelo Trivelli <angelotrivelli@gmail.com>
2021-12-01 14:29:51 -0500

    day 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,18 +1,43 @@
 ﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
-
+        private readonly string input;
+        private readonly int[] depths;
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            input = File.ReadAllText(InputFilePath);
+
+            depths = input
+                        .Split("\r\n")
+                        .Select(d => int.Parse(d))
+                        .ToArray();
         }
 
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        public override ValueTask<string> Solve_1()
+        {
+            var largerTally = 0;
+            for (int i=1 ; i<depths.Length ; i++ )
+            {
+                largerTally += (depths[i] > depths[i-1]) ? 1 : 0;
+            }
+            return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {largerTally}");
+        }
 
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        public override ValueTask<string> Solve_2()
+        {
+            var largerTally = 0;
+            for (int i=0 ; i<depths.Length - 3 ; i++)
+            {
+                largerTally += (depths.Skip(i+1).Take(3).Sum() > depths.Skip(i).Take(3).Sum()) ? 1 : 0;
+            }
+             return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {largerTally}");
+        }
     }
 }
