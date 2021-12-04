121945a1ca8d7e202198450dd77818db8d449ac6
Angelo Trivelli <angelotrivelli@gmail.com>
2021-12-04 12:16:50 -0500

    Day 4

--- AdventOfCode2021/Day_03.cs
+++ AdventOfCode2021/Day_03.cs
@@ -2,6 +2,7 @@ using AoCHelper;
 
 namespace AdventOfCode2021
 {
+
     public class Day_03 : BaseDay
     {
         private readonly string input;
@@ -23,25 +24,12 @@ namespace AdventOfCode2021
                 for(UInt16 i=0 ; i<12 ; i++)
                     tally[i] += (d & (1 << i)) >> i;
 
-            // Console.WriteLine("------------");
-            // foreach(var t in tally){
-            //     Console.Write(t + " ");
-            // }
-            // Console.WriteLine();
-            // Console.WriteLine("------------");
-
             var numLines = diags.Count();
             var g = tally.Select(x => x>numLines/2 ? 1 : 0).Reverse();
             var e = tally.Select(x => x>numLines/2 ? 0 : 1).Reverse();
 
-            Console.WriteLine($"gamma = {string.Join("",g)}");
-            Console.WriteLine($"epsilon = {string.Join("",e)}");
-
             var gamma = Convert.ToUInt16(string.Join("",g), 2);
             var epsilon = Convert.ToUInt16(string.Join("",e), 2);
-
-            Console.WriteLine($"gamma = {gamma}");
-            Console.WriteLine($"epsilon = {epsilon}");
 
             UInt32 product = ((UInt32)gamma)*((UInt32)epsilon);
 
@@ -65,16 +53,10 @@ namespace AdventOfCode2021
                     oxy = oxy.Where(x => ((x & ((UInt16)1 << i)) >> i) == (UInt16)0).ToList();
                 }
 
-                // Console.WriteLine($"{i}--------- oxy count = {oxy.Count()} ");
-                // foreach(var o in oxy)
-                // {
-                //     Console.WriteLine(Convert.ToString(o,2).PadLeft(12,'0'));
-                // }
-                // Console.WriteLine("-----------");
                 if (oxy.Count() == 1) break;
             }
 
-            Console.WriteLine($"oxy = {Convert.ToString(oxy.First(), 2).PadLeft(12, '0')}");
+            // Console.WriteLine($"oxy = {oxy.First()} [{Convert.ToString(oxy.First(), 2).PadLeft(12, '0')}]");
 
             IEnumerable<UInt16> co2 = diags.ToList();
 
@@ -90,16 +72,10 @@ namespace AdventOfCode2021
                     co2 = co2.Where(x => ((x & ((UInt16)1 << i)) >> i) == (UInt16)1).ToList();
                 }
 
-                // Console.WriteLine($"{i}--------- co2 count = {co2.Count()} ");
-                // foreach (var c in co2)
-                // {
-                //     Console.WriteLine(Convert.ToString(c, 2).PadLeft(12, '0'));
-                // }
-                // Console.WriteLine("-----------");
                 if (co2.Count() == 1) break;
             }
 
-            Console.WriteLine($"co2 = {Convert.ToString(co2.First(), 2).PadLeft(12, '0')}");
+            // Console.WriteLine($"co2 = {co2.First()} [{Convert.ToString(co2.First(), 2).PadLeft(12, '0')}]");
 
             return new ValueTask<string>($"oxy * co2 = {oxy.First()*co2.First()}");
         }
---
+++ AdventOfCode2021/Day_04.cs
@@ -0,0 +1,153 @@
+﻿using AoCHelper;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021
+{
+    public class Day_04 : BaseDay
+    {
+        private readonly Regex DrawRegex = new(@"(\d+,)+\d+");
+        private readonly Regex NumberRegex = new Regex(@"\d+");
+        private readonly IEnumerable<int> draws;
+        private readonly IEnumerable<int> allNumbers;
+
+
+        public Day_04()
+        {
+            var input = File.ReadAllText(InputFilePath);
+
+            var drawMatch = DrawRegex.Match(input);
+            var boardStart = drawMatch.Index + drawMatch.Length;
+            var boardNums = input.Substring(boardStart);
+
+            draws = drawMatch.Value.Split(",").Select(x => int.Parse(x));
+            allNumbers = NumberRegex.Matches(boardNums).Select(m => int.Parse(m.Value));
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var numSquaresPerBoard = Board.size * Board.size;
+            var numBoards = allNumbers.Count() / numSquaresPerBoard;
+
+            List<Board> boards = new();
+
+            for (int n=0; n<numBoards; n++)
+            {
+                boards.Add(new Board(n, allNumbers.Skip(n * numSquaresPerBoard).Take(numSquaresPerBoard)));
+            }
+
+            foreach(var d in draws)
+            {
+                foreach (var b in boards)
+                {
+                    if (b.Draw(d))
+                    {
+                        return new ValueTask<string>($"Bingo! Board {b.Id}, draw={d} score={b.Score}, score*draw = {b.Score * d}");
+                    }
+                }
+            }
+
+
+
+            return new ValueTask<string>("");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var numSquaresPerBoard = Board.size * Board.size;
+            var numBoards = allNumbers.Count() / numSquaresPerBoard;
+
+            List<Board> boards = new();
+
+            for (int n = 0; n < numBoards; n++)
+            {
+                boards.Add(new Board(n, allNumbers.Skip(n * numSquaresPerBoard).Take(numSquaresPerBoard)));
+            }
+
+            List<(int id, int draw, int score)> winningBoards = new();
+            foreach (var d in draws)
+            {
+                foreach (var b in boards)
+                {
+                    if (b.Draw(d))
+                    {
+                        winningBoards.Add(new (b.Id, d, b.Score));
+                    }
+                }
+                boards = boards.Where(x => !x.Bingo).ToList(); // remove finished boards
+            }
+
+            var last = winningBoards.Last();
+            return new ValueTask<string>($"Last Bingo!, Board {last.id}, draw={last.draw} score={last.score}, score*draw={last.score * last.draw}");
+        }
+    }
+
+
+    internal class Square
+    {
+        public int Number { get; }
+
+        public int Row { get; }
+        public int Col { get; }
+        public bool IsMarked { get; set; }
+
+        public Square(int number, int row, int col)
+        {
+            Number = number;
+            Row = row;
+            Col = col;
+            IsMarked = false;
+        }
+    }
+
+
+    internal class Board
+    {
+        public static int size = 5;
+        private bool bingo;
+        private int score;
+
+        public int Id { get; }
+        public IList<Square> Squares { get; }
+        public bool Bingo => bingo;
+        public int Score => score;
+
+        public Board(int id, IEnumerable<int> numbers)
+        {
+            if (numbers.Count() != size * size)
+                throw new InvalidOperationException($"need {size * size} numbers for a board!");
+
+            Id = id;
+            Squares = new List<Square>();
+            bingo = false;
+            score = 0;
+
+            int k=0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Squares.Add(new Square(numbers.Skip(k++).Take(1).First(), i, j));
+                }
+            }         
+        }
+
+        public bool Draw(int number)
+        {
+            var matches = Squares.Where(s => s.Number == number);
+
+            foreach (var match in matches)
+            {
+                match.IsMarked = true;
+                
+                if (Squares.Where(s => s.Row == match.Row).All(s => s.IsMarked) ||
+                    Squares.Where(s => s.Col == match.Col).All(s => s.IsMarked))
+                {
+                    bingo = true;
+                    score = Squares.Where(s => s.IsMarked == false).Sum(s => s.Number);
+                }
+            }
+            return bingo;
+        }
+    }
+
+}
