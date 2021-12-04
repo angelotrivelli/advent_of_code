d79b6acce814d99b7937a434cb817ba274a26e73
Angelo Trivelli <angelotrivelli@gmail.com>
2021-12-04 13:06:34 -0500

    more clean up, consolodate solutions 1 and 2

--- AdventOfCode2021/Day_04.cs
+++ AdventOfCode2021/Day_04.cs
@@ -5,77 +5,61 @@ namespace AdventOfCode2021
 {
     public class Day_04 : BaseDay
     {
-        private readonly Regex DrawRegex = new(@"(\d+,)+\d+");
-        private readonly Regex NumberRegex = new Regex(@"\d+");
+        private readonly Regex drawRegex = new(@"(\d+,)+\d+");
+        private readonly Regex numberRegex = new(@"\d+");
+
         private readonly IEnumerable<int> draws;
         private readonly IEnumerable<int> boardNums;
 
+        const int boardSize = 5;
+        const int numSquares = boardSize * boardSize;
+
+        private List<Board> boards = new();
+        List<(int draw, Board board)> winningBoards = new();
 
         public Day_04()
         {
             var input = File.ReadAllText(InputFilePath);
 
-            var drawText = DrawRegex.Match(input);
+            var drawText = drawRegex.Match(input);
             var boardText = input.Substring(drawText.Index + drawText.Length);
 
             draws = drawText.Value.Split(",").Select(x => int.Parse(x));
-            boardNums = NumberRegex.Matches(boardText).Select(m => int.Parse(m.Value));
+            boardNums = numberRegex.Matches(boardText).Select(m => int.Parse(m.Value));
         }
 
 
         public override ValueTask<string> Solve_1()
         {
-            var numSquaresPerBoard = Board.size * Board.size;
-            var numBoards = boardNums.Count() / numSquaresPerBoard;
-
-            List<Board> boards = new();
+            var numBoards = boardNums.Count() / numSquares;
 
             for (int n=0; n<numBoards; n++)
             {
-                boards.Add(new Board(n, boardNums.Skip(n * numSquaresPerBoard).Take(numSquaresPerBoard)));
+                boards.Add(new Board(n, boardSize, boardNums.Skip(n * numSquares).Take(numSquares)));
             }
 
-            foreach(var d in draws)
-            {
-                foreach (var b in boards)
-                {
-                    if (b.Draw(d))
-                    {
-                        return new ValueTask<string>($"Bingo! Board {b.Id}, draw={d} score={b.Score}, score*draw = {b.Score * d}");
-                    }
-                }
-            }
-            return new ValueTask<string>("");
-        }
-
-
-        public override ValueTask<string> Solve_2()
-        {
-            var numSquaresPerBoard = Board.size * Board.size;
-            var numBoards = boardNums.Count() / numSquaresPerBoard;
-
-            List<Board> boards = new();
-
-            for (int n = 0; n < numBoards; n++)
-            {
-                boards.Add(new Board(n, boardNums.Skip(n * numSquaresPerBoard).Take(numSquaresPerBoard)));
-            }
-
-            List<(int id, int draw, int score)> winningBoards = new();
             foreach (var d in draws)
             {
                 foreach (var b in boards)
                 {
                     if (b.Draw(d))
                     {
-                        winningBoards.Add(new (b.Id, d, b.Score));
+                        winningBoards.Add((d, b));
                     }
                 }
-                boards = boards.Where(x => !x.Bingo).ToList(); // remove finished boards
+                boards = boards.Where(x => !x.Bingo).ToList(); // only keep unfinished boards
             }
 
+            var first = winningBoards.First();
+
+            return new ValueTask<string>($"Bingo!, Board {first.board.Id}, draw={first.draw} score={first.board.Score}, score*draw={first.board.Score * first.draw}");
+        }
+
+
+        public override ValueTask<string> Solve_2()
+        {
             var last = winningBoards.Last();
-            return new ValueTask<string>($"Last Bingo!, Board {last.id}, draw={last.draw} score={last.score}, score*draw={last.score * last.draw}");
+            return new ValueTask<string>($"Last Bingo!, Board {last.board.Id}, draw={last.draw} score={last.board.Score}, score*draw={last.board.Score * last.draw}");
         }
     }
 
@@ -99,7 +83,6 @@ namespace AdventOfCode2021
 
     internal class Board
     {
-        public static int size = 5;
         private bool bingo;
         private int score;
 
@@ -108,25 +91,23 @@ namespace AdventOfCode2021
         public bool Bingo => bingo;
         public int Score => score;
 
-        public Board(int id, IEnumerable<int> numbers)
+        public Board(int id, int boardSize, IEnumerable<int> numbers)
         {
-            if (numbers.Count() != size * size)
-                throw new InvalidOperationException($"need {size * size} numbers for a board!");
-
             Id = id;
             Squares = new List<Square>();
             bingo = false;
             score = 0;
 
             int k=0;
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < boardSize; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < boardSize; j++)
                 {
                     Squares.Add(new Square(numbers.Skip(k++).Take(1).First(), i, j));
                 }
             }         
         }
+
 
         public bool Draw(int number)
         {
@@ -146,5 +127,4 @@ namespace AdventOfCode2021
             return bingo;
         }
     }
-
 }
