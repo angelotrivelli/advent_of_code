f951541b1ff1f2f606db3b2ee8aae446cd19236f
Angelo Trivelli <angelotrivelli@gmail.com>
2021-12-06 17:56:01 -0500

    day 06 done!

---
+++ AdventOfCode2021/Day_07.cs
@@ -0,0 +1,26 @@
+using AoCHelper;
+
+namespace AdventOfCode2021
+{
+    public class Day_07 : BaseDay
+    {
+
+        public readonly string input;
+
+        public Day_07()
+        {
+            input = File.ReadAllText(InputFilePath);
+
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new ValueTask<string>("");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new ValueTask<string>("");
+        }
+    }
+}
